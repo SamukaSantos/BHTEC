379294ae30e2db0e667ed5144765e36141610466
André C. O. Minelli <andre@takenet.com.br>
2015-04-18 13:43:55 -0300

    Implemented GitService

--- Sample/Sample/Sample/Service/GitService.cs
+++ Sample/Sample/Sample/Service/GitService.cs
@@ -1,7 +1,16 @@
-﻿
+﻿using Octokit;
+using System.Threading.Tasks;
+
 namespace Sample.Service
 {
     public class GitService
     {
+        public Task<User> GetUserAsync (string username)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("DesafioMeetup"));
+            var user = client.User.Get(username);
+
+            return user;
+        }
     }
 }
