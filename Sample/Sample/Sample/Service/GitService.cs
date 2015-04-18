8c5b9524aa15f9411951ce3b7a0c441ad4034630
Administrador <administrador@MacBook-Pro-de-Administrador.local>
2015-04-18 13:34:56 -0300

    Novos arquivos

--- Sample/Sample/Sample/Service/GitService.cs
+++ Sample/Sample/Sample/Service/GitService.cs
@@ -1,7 +1,37 @@
-﻿
+﻿using System.Net;
+using System;
+using Xamarin.Forms;
+
+
 namespace Sample.Service
 {
     public class GitService
     {
+
+
+		public void User GetUser(string user)
+		{
+	
+
+			WebRequest requestPic = WebRequest.Create(string.Format("https://api.github.com/users/{0}",user));
+			object request;
+			requestPic.BeginGetResponse (FeedDownloaded, request);
+
+		}
+
+		void FeedDownloaded (IAsyncResult result)
+		{
+
+			var request = result.AsyncState as HttpWebRequest;
+
+			try {
+				var response = request.EndGetResponse (result);
+				//(response.GetResponseStream ()).re;
+			//	Newtonsoft.Json.JsonConvert.DeserializeObject<User>(response.GetResponseStream());
+		
+			} catch (Exception e) {
+				
+			}
+		}
     }
 }
