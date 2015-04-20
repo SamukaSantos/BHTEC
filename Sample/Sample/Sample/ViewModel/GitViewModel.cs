d2b0f22eed89527bc821217dc63956ea1e3de6c3
André C. O. Minelli <andre@takenet.com.br>
2015-04-20 09:16:48 -0300

    Using Fody.PropertChanged on GitViewModel(https://github.com/Fody/PropertyChanged), in order to cleanup the model

--- Sample/Sample/Sample/ViewModel/GitViewModel.cs
+++ Sample/Sample/Sample/ViewModel/GitViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Sample.Service;
+﻿using PropertyChanged;
+using Sample.Service;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -12,54 +13,18 @@ using Xamarin.Forms;
 
 namespace Sample.ViewModel
 {
-	public class GitViewModel : INotifyPropertyChanged
+	[ImplementPropertyChanged]
+	public class GitViewModel
 	{
-
-		#region Fields
-
-		private string _userName;
-
-		private string _email;
-
-		private string _login;
-
-		#endregion
-
 		#region Properties
 
-		public GitService Service { get; set; }
+		public GitService Service { get; private set; }
 
-		public string UserName 
-		{
-			get { return _userName;  }
-			set 
-			{
-				_userName = value;
-				RaisedPropertyChanged(() => UserName);
-			}
-		}
+		public string UserName { get; set; }
 
+		public string Email { get; set; }
 
-		public string Email 
-		{
-			get { return _email;  }
-			set 
-			{
-				_email = value;
-				RaisedPropertyChanged(() => Email);
-			}
-		}
-
-
-		public string Login 
-		{
-			get { return _login;  }
-			set 
-			{
-				_login = value;
-				RaisedPropertyChanged(() => Login);
-			}
-		}
+		public string Login { get; set; }
 
 		#endregion
 
@@ -75,7 +40,6 @@ namespace Sample.ViewModel
 							}); 
 			}
 		}
-
 
 		public void GetUser()
 		{
@@ -93,27 +57,6 @@ namespace Sample.ViewModel
 		public GitViewModel()
 		{
 			Service = new GitService();
-		}
-
-		#endregion
-
-		#region Events
-		public event PropertyChangedEventHandler PropertyChanged;
-		#endregion
-
-
-		#region Methods
-
-		protected void RaisedPropertyChanged<PropertyType>(Expression<Func<PropertyType>> property)
-		{
-			var memberExpression = property.Body as MemberExpression;
-			var propertyInfo = memberExpression.Member as PropertyInfo;
-
-			if (propertyInfo != null)
-			{
-				if (PropertyChanged != null)
-					PropertyChanged(this, new PropertyChangedEventArgs(propertyInfo.Name));
-			}
 		}
 
 		#endregion
