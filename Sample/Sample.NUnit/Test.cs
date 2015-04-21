f515d632c84c651e5e4478be250bc0b4ee1d9b3b
Administrador <administrador@MBPdeAdistrador.home>
2015-04-21 18:55:08 -0300

    Inclusão de  referências http no projeto IOS. Implementado teste unitário para o serviço git.

---
+++ Sample/Sample.NUnit/Test.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using Sample.Service;
+using Sample.Service.Creator;
+
+namespace Sample.NUnit
+{
+	[TestFixture ()]
+	public class Test
+	{
+
+		string userValid = "dionisoliveira" ;
+
+		string userInvalid = "123BHTECK" ;
+
+
+		[Test ()]
+		public void UserValidProfile ()
+		{
+			var Service = ServiceCreator.Get<GitService>();
+
+			Assert.IsNotNull(Service.GetUserAsync(userValid).Result);
+
+				
+		
+		}
+
+		[Test ()]
+		public void UserInvalidProfile ()
+		{
+			var Service = ServiceCreator.Get<GitService>();
+
+			Assert.IsNotNull(Service.GetUserAsync(userInvalid).Result);
+
+
+
+		}
+	}
+}
+
